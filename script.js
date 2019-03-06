1406c44ac010fae6ab99df36e8fed28863bc77a4
christianc345 <christian_curiel@davincischools.org>
2019-03-06 14:39:34 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+ram(){
+	document.getElementById("q1").src("rams.jpg");
+}
+
+charger(){
+	document.getElementById("q1").src("chargers.jpg");
+}
+
+raiders(){
+	document.getElementById("q1").src("raider.jpg");
+}
+
+miamid(){
+	document.getElementById("q1").src("miami.png");
+}
+
+steeler(){
+	document.getElementById("q1").src("steelers.jpg");
+}
+
+vikings(){
+	document.getElementById("q1").src("vikings.jpg");
+}
